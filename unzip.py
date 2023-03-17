d757d37b21e23dd75301d5f059fd5423a26ae26d
Y30L <101905747+byeongyeolahn@users.noreply.github.com>
2023-03-17 18:10:11 +0900

    압축파일 해제 코드(Python)
    압축파일 해제 코드(Python)

--- unzip.py
+++ unzip.py
@@ -1,26 +1,15 @@
-import os
-import zipfile
-import traceback
+import pyzipper
 
-def Unzip_fun(path):
-    if os.path(path):
-        # 파일 목록 획득
-        file_list = os.listdir(path)
-        file_list_zip = [file for file in file_list if file.endswith(".zip")]
-
-        # 각 파일 압축해제 루틴
-        for i in range(len(file_list_zip)):
-            full_path = path + file_list_zip[i]
-            # 압축 파일 오픈
-            zip_file = open(full_path, 'rb')
-            Target_zip = zipfile.ZipFile(zip_file)
-            #패스워드 지정
-            Target_zip.setpassword('지정된 비밀번호')
-            for k in Target_zip.namelist():
-                try:
-                    Target_zip.extract(k, path+"\\"+file_list_zip[i])
-                except Exception:
-                    traceback.print_exc()
-            zip_file.close()
-    else:
-        print("존재하지 않는 경로")
+def _unzip():
+    file_list = os.listdir(압축해제 대상 파일이 존재하는 폴더)
+    try:
+        print("[+] 압축해제 시작")
+        for ef in file_list:
+            unzip.unzip_fun(f"압축해제 대상 파일이 존재하는 폴더\\{ef}")
+        print("[+] 압축해제 완료!")
+    except:
+        print("[+] 압축해제 대상 파일이 존재하지 않습니다.")
+        
+      
+if __name__=="__main__":
+    _unzip()
